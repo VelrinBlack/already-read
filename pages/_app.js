be27d920060439834e5038ba7fd2fa01220282be
Velrin Black <velrinblack@gmail.com>
2022-01-25 10:18:11 +0100

    Set up absolute imports, styled-components, theme and global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+import GlobalStyle from 'styles/globalStyle';
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
---
+++ styles/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  fontSize: {
+    XS: '14px',
+    S: '18px',
+    M: '24px',
+    L: '36px',
+    XL: '48px',
+  },
+  color: {
+    white: '#FFFFFF',
+    black: '#000000',
+    yellow: '#FDCA40',
+    blue: '#33A1FD',
+    lightGrey: '#545D60',
+    darkGrey: '#31393C',
+  },
+};
+
+export default theme;
